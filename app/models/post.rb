ca83a27748105ffab77a5a4e43cbdae951cc9bb0
Shawn Ren <shawnren527@gmail.com>
2019-05-01 21:28:18 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,3 @@
-class MyValidator < ActiveModel::Validator
-  def validate(post)
-    unless post.title.match?('Secret') || post.title.match?('Top[number]') || post.title.match?('Guess')
-      post.errors[:title] << 'Wrong'
-    end
-  end
-end
-
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
