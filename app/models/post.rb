8ed62559d13dd60c70bd06e93f3f741749a531df
Shawn Ren <shawnren527@gmail.com>
2019-05-01 21:39:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
-  validates :is_clickbaity(post)
+  validates :is_clickbaity
 
-  def is_clickbaity(post)
-    unless post.title.match?("Won't believe") || post.title.match?('Secret') || post.title.match?('Top[number]') || post.title.match?('Guess')
-      post.errors << 'must be clickbait'
+  def is_clickbaity
+    unless self.title.match?("Won't believe") || self.title.match?('Secret') || self.title.match?('Top[number]') || self.title.match?('Guess')
+      self.errors << 'must be clickbait'
     end
   end
 end
