f9e52a75ed85a92ca58e98c34765471ab5480d39
Shawn Ren <shawnren527@gmail.com>
2019-05-01 21:25:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class MyValidator < ActiveModel::Validator
   def validate(post)
     unless post.title.match?('Secret') || post.title.match?('Top[number]') || post.title.match?('Guess')
       post.errors[:title] << 'Wrong'
+    end
+  end
+end
 
 class Post < ActiveRecord::Base
   validates :title, presence: true
